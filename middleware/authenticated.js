9d4cc15d45c9a93230ab52e7246e09e33e3dc9f7
ma-ryu <54539186+ma-ryu@users.noreply.github.com>
2020-07-27 16:21:20 +0900

    静的ファイルを生成　デプロイして公開

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,10 +1,15 @@
-import { firebase } from '~/plugins/firebase'
+import { firebase, db } from '~/plugins/firebase'
 
 export default function ({ route, store, redirect }) {
   // ユーザーが認証されていないとき
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       store.dispatch('setUser', user)
+      db.collection('profiles').doc(user.uid).set({
+        uid: user.uid,
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+      })
     } else if (route.path !== '/login') redirect('/login')
   })
 }
