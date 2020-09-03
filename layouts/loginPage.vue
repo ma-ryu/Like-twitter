b08a4e07de96a77d75da722b41707b9e1023669d
ma-ryu <54539186+ma-ryu@users.noreply.github.com>
2020-09-03 22:53:21 +0900

    ログイン画面の修正

---
+++ layouts/loginPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app dark>
+    <main>
+      <v-container>
+        <nuxt />
+      </v-container>
+      <bottomNav v-if="$vuetify.breakpoint.smAndDown" />
+    </main>
+    <v-footer class="text-center">
+      <span class="mx-auto">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import bottomNav from '~/components/bottomNav.vue'
+
+export default {
+  components: { bottomNav },
+  middleware: 'authenticated',
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+  methods: {
+    close() {
+      this.dialog = false
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,14 +1,74 @@
 <template>
-  <form>
-    <v-text-field :counter="10" label="Name" required></v-text-field>
-    <v-text-field label="E-mail" required></v-text-field>
-    <v-btn class="mr-4">submit</v-btn>
-  </form>
+  <v-card>
+    <v-card-title class="justify-center text-h4">
+      Log in for warble
+    </v-card-title>
+    <v-img
+      height="300px"
+      width="300px"
+      class="mx-auto"
+      :src="require('@/assets/img/warble.png')"
+    ></v-img>
+    <v-card-actions>
+      <v-btn
+        v-if="!isAuthenticated"
+        rounded
+        class="pa-5 mb-5 d-flex mx-auto ma-5"
+        @click="login"
+      >
+        <v-icon small class="mr-5 primary--text">mdi-google</v-icon>Log in
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+import { firebase } from '~/plugins/firebase'
+
 export default {
   middleware: 'authenticated',
+  layout: 'loginPage',
+  computed: {
+    user() {
+      return this.$store.state.user
+    },
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated
+    },
+  },
+  methods: {
+    ...mapActions(['setUser']),
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          const user = result.user
+          this.setUser(user)
+          console.log(this.$store.state.user)
+          this.$router.push('/')
+        })
+        .catch((error) => {
+          window.alert(error)
+        })
+    },
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.setUser(null)
+          window.alert('ログアウトに成功！')
+          this.$router.push('/login')
+        })
+        .catch((e) => {
+          window.alert('ログアウトに失敗しました')
+          console.log(e)
+        })
+    },
+  },
 }
 </script>
 
